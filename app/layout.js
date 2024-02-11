4abf678f07867656371e5ff7add8da25de84bf87
mosespace <kisakyemoses50@gmail.com>
2024-02-11 16:20:13 +0300

    feat:added my tasks @mosespace

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google";
-import "../styles/global.scss";
+import "../styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>{children}</body>
     </html>
   );
